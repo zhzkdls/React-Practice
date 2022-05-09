2b04475b50fcadbe363167e1fd1ee3d724c72cb4
zhzkdls <gkdud3558@naver.com>
2022-05-09 23:03:46 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './App.css';
+import ImportConponent from './components/ImportConponent';
+import LifecycleEx from './components/LifecycleEx';
 
 function App() {
   return (
     <div>
-      <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
+      <h1>choma React</h1>
+      <p>CSS 적용하기</p>
+      <ImportConponent />
+      <LifecycleEx />
     </div>
   );
 }
---
+++ src/components/LifecycleEx.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LifecycleEx() {
+
+    console.log('3. render Call');
+    return ( 
+        <div>
+            <h2>[ 김하영 ]</h2>
+        </div>
+     );
+}
+
+export default LifecycleEx;

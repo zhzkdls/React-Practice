f2d885279762843871af8b67759a860d7bafc85c
zhzkdls <gkdud3558@naver.com>
2022-05-28 01:12:00 +0900

    Update

--- React-02/src/App.js
+++ React-02/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import StrAddButton from './StrAddButton'
 
 class App extends Component {
@@ -6,11 +7,22 @@ class App extends Component {
         return (
             <div>
                 <h1>Start React 200!</h1>
-                <span>{this.props.store.getState().data.str}</span><br/>
-                <StrAddButton store={this.props.store}/>
+                {/* <span>{this.props.store.getState().data.str}</span><br/> */}
+                <span>{this.props.str}</span><br/>
+                {/* <StrAddButton store={this.props.store}/> */}
+                <StrAddButton AppProp="200"/>
             </div>
         );
     }
 }
+
+let mapStateToProps = (state, props) => {
+    console.log('Props from indes.js : ' + props.indexProp)
+    return {
+        str: state.data.str,
+    };
+};
+
+App = connect(mapStateToProps, null)(App);
 
 export default App;
